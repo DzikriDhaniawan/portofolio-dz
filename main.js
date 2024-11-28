c75fb77d1daa3e47961703a7848382f2fb4780fa
Dzikri Dhaniawan <dhaniawan.dzikri@gmail.com>
2024-11-28 12:43:57 +0800

    update

--- main.js
+++ main.js
@@ -15,10 +15,25 @@ function showSlides() {
     }
     slides[slideIndex - 1].style.display = "block";  
     dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 5000);
+    setTimeout(showSlides, 3500);
 }
 
 function currentSlide(n) {
     slideIndex = n - 1;
     showSlides();
 }
+
+let skillsSlideIndex = 0;
+const skillsSlides = document.querySelectorAll(".skills-slide");
+
+function showSkillsSlides() {
+    skillsSlides.forEach((slide, index) => {
+        slide.classList.remove("active");
+        if (index === skillsSlideIndex) slide.classList.add("active");
+    });
+
+    skillsSlideIndex = (skillsSlideIndex + 1) % skillsSlides.length;
+    setTimeout(showSkillsSlides, 3000);
+}
+
+showSkillsSlides();
